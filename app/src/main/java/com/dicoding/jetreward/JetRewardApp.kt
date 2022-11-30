046accf1fd097b7f28dbded674caa5b7978660e8
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-11-30 16:34:56 +0700

    add navigation on bottom bar

--- app/src/main/java/com/dicoding/jetreward/JetRewardApp.kt
+++ app/src/main/java/com/dicoding/jetreward/JetRewardApp.kt
@@ -7,13 +7,16 @@ import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.dicoding.jetreward.ui.navigation.NavigationItem
 import com.dicoding.jetreward.ui.navigation.Screen
@@ -59,6 +62,8 @@ private fun BottomBar(
     BottomNavigation(
         modifier = modifier
     ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
         val navigationItems = listOf(
             NavigationItem(
                 title = stringResource(R.string.menu_home),
@@ -86,8 +91,16 @@ private fun BottomBar(
                         )
                     },
                     label = { Text(item.title) },
-                    selected = true,
-                    onClick = {}
+                    selected = currentRoute == item.screen.route,
+                    onClick = {
+                        navController.navigate(item.screen.route) {
+                            popUpTo(navController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+                            restoreState = true
+                            launchSingleTop = true
+                        }
+                    }
                 )
             }
         }
